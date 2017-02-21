cbecd52409522b9f4dd24c6335414c8858ed2948
douduck08 <lu10082000@gmail.com>
2017-02-21 23:40:31 +0800

    commit all

---
+++ Assets/TestUnit.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestUnit : MonoBehaviour {
+
+    bool first = true;
+    bool firstlate = true;
+
+    void Awake() {
+        Debug.Log(name + " Awake");
+    }
+
+    void Start () {
+        Debug.Log(name + " Start " + Time.frameCount);
+    }
+
+    void OnEnable() {
+        Debug.Log(name + " OnEnable");
+    }
+
+    void OnDisable() {
+        Debug.Log(name + " OnDisable" );
+    }
+
+    void Update() {
+        if (first) {
+            first = false;
+            Debug.Log(name + " Update " + Time.frameCount);
+        }
+    }
+
+    void LateUpdate() {
+        if (firstlate) {
+            firstlate = false;
+            Debug.Log(name + " LateUpdate " + Time.frameCount);
+        }
+    }
+
+    void OnDestroy() {
+        Debug.Log(name + " OnDestroy");
+    }
+
+    void OnApplicationQuit() {
+        Debug.Log(name + " OnApplicationQuit");
+    }
+}
---
+++ Assets/Tester.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tester : MonoBehaviour {
+
+    public GameObject UnitPrefab;
+    private GameObject tmp;
+    bool first = true;
+
+    void Awake() {
+        // Debug.Log("=== Instantiate in Awake ===");
+        // tmp = GameObject.Instantiate(UnitPrefab);
+        // Debug.Log("=== Instantiate in Awake ===");
+
+        // Debug.Log("=== Destroy in Awake ===");
+        // GameObject.Destroy(tmp);
+        // Debug.Log("=== Destroy in Awake ===");
+    }
+
+    void Start () {
+        // Debug.Log("=== Instantiate in Start ===");
+        // tmp = GameObject.Instantiate(UnitPrefab);
+        // Debug.Log("=== Instantiate in Start ===");
+
+        // Debug.Log("=== Destroy in Start ===");
+        // GameObject.Destroy(tmp);
+        // Debug.Log("=== Destroy in Start ===");
+    }
+
+    void Update() {
+        // if (first) {
+        //     first = false;
+        //     Debug.Log("=== Instantiate in Update ===");
+        // tmp = GameObject.Instantiate(UnitPrefab);
+        // Debug.Log("=== Instantiate in Update ===");
+        // 
+        // Debug.Log("=== Destroy in Update ===");
+        // GameObject.Destroy(tmp);
+        // Debug.Log("=== Destroy in Update ===");
+        // }
+    }
+
+    public void ReloadScene() {
+        Debug.Log("=== ReloadScene Start ===");
+        StartCoroutine(StartLoadScene("main"));
+    }
+
+    private IEnumerator StartLoadScene(string sSceneName) {
+        AsyncOperation op = SceneManager.LoadSceneAsync(sSceneName, LoadSceneMode.Single);
+        while (!op.isDone) {
+            yield return new WaitForEndOfFrame();
+            Debug.Log("=== ReloadScene ing ===");
+        }
+        Debug.Log("=== ReloadScene End ===");
+    }
+}
